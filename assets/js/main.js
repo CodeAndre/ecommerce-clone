99cd07d0db1806c070b7e9ae937d3895ef8a14d8
Andre Viana <code.andre3@gmail.com>
2021-06-15 19:08:01 -0300

    put featured

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,9 +35,9 @@ function scrollActive() {
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelecto('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         } else  {
-            document.querySelecto('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
